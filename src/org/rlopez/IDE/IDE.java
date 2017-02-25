672a6b7ac150e3e097c73680f4abe05bf95d9047
ghostzer <djxrico@gmail.com>
2017-02-25 15:30:59 +0100

    add gui exercice

---
+++ src/org/rlopez/IDE/IDE.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rlopez.IDE;
+
+import java.util.Scanner;
+import org.rlopez.IDE.models.MainWindow;
+
+/**
+ *
+ * @author rico
+ */
+public class IDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MainWindow mainwindow = new MainWindow();
+        mainwindow.setVisible(true);
+
+    }
+}
